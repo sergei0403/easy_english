4672304d48c35536b9d6ca3d75f68c7128cd805f
Sergei Sorochinskii <sergei.sorochinskii@dteam.dev>
2024-04-01 20:30:46 +0300

    Fixed flake8 part 5

--- backend/src/app/core/database.py
+++ backend/src/app/core/database.py
@@ -20,14 +20,16 @@ class AsyncDatabaseSession:
             future=True,
             echo=True,
         )
-        self._session = sessionmaker(self._engine, expire_on_commit=False, class_=AsyncSession)()
+        self._session = sessionmaker(
+            self._engine, expire_on_commit=False, class_=AsyncSession
+        )()
 
     async def create_all(self):
         async with self._engine.begin() as conn:
             await conn.run_sync(Base.metadata.create_all)
 
 
-class TestDatabaseSession():
+class TestDatabaseSession:
     def __init__(self):
         self._session = None
         self._engine = None
@@ -40,7 +42,9 @@ class TestDatabaseSession():
             "sqlite+aiosqlite:///:memory:",
             echo=True,
         )
-        self._session = sessionmaker(self._engine, class_=AsyncSession, expire_on_commit=False)()
+        self._session = sessionmaker(
+            self._engine, class_=AsyncSession, expire_on_commit=False
+        )()
 
     async def create_all(self):
         async with self._engine.begin() as conn:
@@ -48,8 +52,6 @@ class TestDatabaseSession():
 
 
 if settings.IS_TEST_DATABASE:
-    print(f"{settings.IS_TEST_DATABASE=}")
     db = TestDatabaseSession()
 else:
-    print(f"{settings.IS_TEST_DATABASE=}") 
-    db = AsyncDatabaseSession()    
+    db = AsyncDatabaseSession()
